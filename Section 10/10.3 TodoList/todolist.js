b1a139a231350add8f0fe56d04cf7eb80ce14b1d
MichielViss <30391430+MichielViss@users.noreply.github.com>
2018-10-30 14:07:33 +0200

    Updated todoList with the delete functionality
    Test on this entire GitHub nonsense

--- Section 10/10.3 TodoList/todolist.js
+++ Section 10/10.3 TodoList/todolist.js
@@ -1,4 +1,3 @@
-
 window.setTimeout(function() {
 var input=prompt("Please type what you would like to form the options listed on the webpage");
 var todo=[];
@@ -12,18 +11,18 @@ while(input!="quit"){
 		inputNew = prompt("Add the text for you new Todo item");
 		todo[i] = inputNew;
 		i=i+1;
-	}
-		
-
-	if(input==="list"){
+	}else if(input==="list"){
 		console.log("**********");
 		todo.forEach(function(el,li){
-
 			// the function arguments are strange on this one:
 			// the first argument is the element value and the second one is the index of the element. 
 			console.log	((li+1)+": " + el); //This is how we print the index and then the element for each element in the array along with the index at which it is saved
 		});
 		console.log("**********");
+	}else if(input ==="delete"){
+		var index=prompt("Enter the index of the todo item to delete");
+		//splice is the array property used to delete specific indexes from an array
+		todo.splice(index,1);
 	}
 	input=prompt("Please type what you would like to form the options listed on the webpage");
 }
